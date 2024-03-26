3294abcf81e0b719b2c82267e13d02a9f29edbb5
dassepascal <pascaldasse56@gmail.com>
2024-03-26 10:08:12 +0100

    vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,14 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import Test from './components/Test.vue';
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const app = createApp({});
+
+app.component('test', Test);
+app.mount('#app');
---
+++ resources/js/components/Test.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-red-500">
+        <h1>Test Component</h1>
+    </div>
+</template>
